13e25e4c24b6a6f00ac2a74f8e16e285dd449cd6
longhoang2984 <longhoang.2984@gmail.com>
2016-05-30 17:36:41 +0700

    [ADD] splash screen & [MOD] launch screen

---
+++ IDEA Euro 2016/Controllers/SplashViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SplashViewController.swift
+//  IDEA Euro 2016
+//
+//  Created by Cuu Long Hoang on 5/30/16.
+//  Copyright © 2016 Long Hoang. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var OLDanhSachDoi: UIButton!
+    @IBOutlet weak var OLLichThiDau: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        radiusBorder()
+    }
+    
+    func radiusBorder(){
+        OLLichThiDau.layer.cornerRadius = 10
+        OLDanhSachDoi.layer.cornerRadius = 10
+        OLLichThiDau.layer.masksToBounds = true
+        OLDanhSachDoi.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
