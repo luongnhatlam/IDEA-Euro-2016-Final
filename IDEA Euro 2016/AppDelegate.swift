a85cb57ea605aa35ef7668ce7a4fb810dde56e70
Tran Xuan Viet <viettranx@gmail.com>
2016-05-31 12:10:18 +0700

    [MOD] - Remove setting color for NavBar

--- IDEA Euro 2016/AppDelegate.swift
+++ IDEA Euro 2016/AppDelegate.swift
@@ -18,9 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
-        UINavigationBar.appearance().backgroundColor = UIColor(red: 22/255, green: 131/255, blue: 198/255, alpha: 1)
+        //UINavigationBar.appearance().backgroundColor = UIColor(red: 22/255, green: 131/255, blue: 198/255, alpha: 1)
 //        UINavigationBar.appearance().setTra
-        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        //UINavigationBar.appearance().tintColor = UIColor.whiteColor()
         return true
     }
 
