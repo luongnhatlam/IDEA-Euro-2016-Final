f5ef38db71092728d4ef128915cf93b885018644
longhoang2984 <longhoang.2984@gmail.com>
2016-06-07 09:49:24 +0700

    [MOD]

--- IDEA Euro 2016/Controllers/TeamsViewController.swift
+++ IDEA Euro 2016/Controllers/TeamsViewController.swift
@@ -13,6 +13,7 @@ class TeamsViewController: UIViewController {
     
     var database:FIRDatabaseReference!
     var dataList:[[String:[Team]]] = [[String:[Team]]]()
+    
     var loadingState:UIView = UIView()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -37,13 +38,16 @@ class TeamsViewController: UIViewController {
         loadingState = LoadingState.shareInstance.createLoadingState()
         self.view.addSubview(loadingState)
         tableView.userInteractionEnabled = false
+        
         self.database.child("Groups").queryOrderedByKey().observeEventType(.Value,withBlock: { (snap) in
             
+            
             if snap.value != nil {
-                
+                print(snap.value)
                 self.dataList.removeAll()
                 
                 if let list = snap.value as? [String:AnyObject] {
+                    
                     let sortList = list.sort() { $0.0  < $1.0  }
                     
                     for (groupName, teams) in sortList {
@@ -51,6 +55,7 @@ class TeamsViewController: UIViewController {
                         var teamList = [Team]()
                         
                         if let teamsData = teams as? [String:[String:AnyObject]] {
+                            
                             for (teamName, teamInfo) in teamsData {
                                 
                                 guard let teamPoint = teamInfo["Point"] as? Int else { continue }
@@ -62,6 +67,7 @@ class TeamsViewController: UIViewController {
                             }
                         }
                         
+                        
                         if teamList.count > 0 {
                             let groupAndTeam = [ "Bảng \(groupName)" : teamList ]
                             self.dataList.append(groupAndTeam)
@@ -70,6 +76,8 @@ class TeamsViewController: UIViewController {
                     
                     dispatch_async(dispatch_get_main_queue(), {
                         self.tableView.reloadData()
+                        
+                        
                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(UInt64(1) * NSEC_PER_SEC)), dispatch_get_main_queue()) {
                             
                             UIView.animateWithDuration(0.25, animations: {
@@ -86,42 +94,6 @@ class TeamsViewController: UIViewController {
         })
     }
     
-    func valueChanged() {
-        self.database.child("Groups").queryOrderedByKey().observeEventType(.ChildChanged,withBlock: { (snap) in
-            if snap.value != nil {
-                if let list = snap.value as? [String:AnyObject] {
-                    let sortList = list.sort() { $0.0  < $1.0  }
-                    for (groupName,teams) in sortList {
-                        if let teamsData = teams as? [String:AnyObject] {
-                            var teamList:[Team] = [Team]()
-                            for (teamName,teamInfoData) in teamsData {
-                                var data:[String:AnyObject] = [
-                                    "name" : teamName
-                                ]
-                                if let teamInfo = teamInfoData as? [String:AnyObject] {
-                                    data["point"] = teamInfo["Point"]
-                                    data["rank"] = teamInfo["Rank"]
-                                }
-                                if let team = Team(data: data) {
-                                    teamList.append(team)
-                                }
-                            }
-                            if teamList.count > 0 {
-                                let group = [groupName:teamList]
-                                self.dataList.append(group)
-                            }
-                            
-                        }
-                    }
-                    
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.tableView.reloadData()
-                    })
-                }
-            }
-        })
-        
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -130,13 +102,18 @@ class TeamsViewController: UIViewController {
 }
 
 extension TeamsViewController : UITableViewDataSource {
+    
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return self.dataList.count
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         var row = 0
-        for (_,teamList) in self.dataList[section] {
+        let thongTinBangDau = self.dataList[section]
+        
+        for (_,teamList) in thongTinBangDau {
             if let teams:[Team] = teamList {
                 row = teams.count
             }
@@ -171,21 +148,27 @@ extension TeamsViewController : UITableViewDataSource {
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
     {
         let headerView = UIView(frame: CGRectMake(0, 0, tableView.bounds.size.width, 40))
-        let bg = UIImageView(frame: headerView.frame)
+        
+        let bg = UIImageView(frame: headerView.bounds)
         bg.image = UIImage(named: "board")
         bg.contentMode = .ScaleAspectFill
+        
         headerView.addSubview(bg)
+        
         let textLabel = UILabel(frame: CGRect(x: 16, y: 0, width: 200, height: 40 ))
         textLabel.font = UIFont(name: "Avenir Next Heavy", size: 25)
         textLabel.textColor = UIColor.whiteColor()
+        headerView.addSubview(textLabel)
+        
         let group = self.dataList[section]
-        for (groupName, _) in group {
-            textLabel.text = groupName
-            headerView.addSubview(textLabel)
-        }
+        textLabel.text = group.keys.first
         return headerView
     }
 }
+
+
+
+
 
 extension TeamsViewController : UITableViewDelegate {
     
@@ -194,7 +177,7 @@ extension TeamsViewController : UITableViewDelegate {
         let group = self.dataList[indexPath.section]
         
         for (_, teams) in group {
-            var teamsArr = teams.sort() { $0.point > $1.point }
+            var teamsArr = teams.sort() { $0.rank < $1.rank }
             
             let team:Team = teamsArr[indexPath.row]
             
